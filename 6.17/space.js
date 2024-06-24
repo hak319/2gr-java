290325f01d761196214494610755c9ff48d5ab3f
Hak319 <134386603+hak319@users.noreply.github.com>
2024-06-24 12:35:53 +0900

    Add files via upload

---
+++ 6.17/space.js
@@ -0,0 +1,26 @@
+let player1 = new Object();
+player1.name = 'test1'
+player1.job = '노숙자'
+player1.race = '엘프'
+player1. stats ={
+    STR: 8,
+    DEX: 15,
+    CON: 14,
+    INT: 10,
+    WIS: 14,
+    CHA: 8
+}
+
+let player2 = {
+    name: 'test2',
+    job: '노숙자',
+    race: '엘프',
+    stats: {
+        STR: 8,
+        DEX: 15,
+        CON: 14,
+        INT: 10,
+        WIS: 14,
+        CHA: 8
+    }
+}
